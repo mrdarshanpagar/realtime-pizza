ab32112788083dc5c688f6d8a9c1182c0b5d6413
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-09-07 23:10:54 +0530

    created folder structure

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+const PORT = process.env.PORT || 3300
+
+// app.use(expressLayout)
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, '/resources/views'))
+
+app.get('/', (req,res)=>{
+    res.render('home.ejs')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server on port no: ${PORT}`)
+})
