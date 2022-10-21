a3cf8c48558f2f251088a057645ef02f4405ceb5
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-10-21 22:10:39 +0530

    added login,register and cart pages

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3300
 
-// app.use(expressLayout)
+app.use(expressLayout)
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, '/resources/views'))
 
@@ -15,6 +15,19 @@ app.use(express.static('public'))
 app.get('/', (req,res)=>{
     res.render('home.ejs')
 })
+
+app.get('/cart', (req,res)=>{
+    res.render('customers/cart.ejs')
+})
+
+app.get('/login', (req,res)=>{
+    res.render('auth/login.ejs')
+})
+
+app.get('/register', (req,res)=>{
+    res.render('auth/register.ejs')
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Server on port no: ${PORT}`)
